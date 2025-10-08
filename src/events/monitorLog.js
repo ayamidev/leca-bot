fa9d7c457881646f1af5752ac7977d5bb02aecae
aya <matsudaayami@gmail.com>
2025-10-08 00:12:30 -0300

    Leca 20251007 - Correção do monitor do chat da Loritta

--- src/events/monitorLog.js
+++ src/events/monitorLog.js
@@ -1,11 +1,16 @@
+import { Server } from "../models/server.js";
 import { embedContemLeca } from "../utils/embeds.js";
-import { config } from "../config.js";
 
 export async function onMonitorLog(message, client) {
-  const { defaultLogChannelId } = config;
-  if (!defaultLogChannelId || !message.guild) return;
-  if (message.channel.id !== defaultLogChannelId) return;
+  if (!message.guild) return;
   if (message.author.id === client.user.id) return;
+
+  // --- PEGAR O CANAL DE MONITORAMENTO DO BANCO ---
+  const server = await Server.findOne({ guildId: message.guild.id });
+  if (!server || !server.channels.defaultLogChannelId) return;
+
+  const defaultLogChannelId = server.channels.defaultLogChannelId;
+  if (message.channel.id !== defaultLogChannelId) return;
 
   console.log(`[DEBUG] Mensagem em #${message.channel.name} por ${message.author.tag}:`, message.content);
 
