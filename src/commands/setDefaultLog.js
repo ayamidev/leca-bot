964a32e0704b77e3048a715049bc9f79bb6d61ae
aya <matsudaayami@gmail.com>
2025-10-07 23:10:05 -0300

    Leca 20251007 - Instanciamento do banco de dados MongoDB para preservar informações dos Servers em que a Leca for adicionada, troca do comando /excluir_papinhos_furados para /purge papinhos e /purge all papinhos com identificação de mensagens encaminhadas v2

--- src/commands/setDefaultLog.js
+++ src/commands/setDefaultLog.js
@@ -1,4 +1,4 @@
-import { Server } from "../models/Server.js";
+import { Server } from "../models/server.js";
 
 export async function handleSetDefaultLog(interaction) {
   if (!interaction.member.permissions.has("Administrator")) {
